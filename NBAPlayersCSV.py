22a80e228d68a86a92d5ce623a048ce6fd3d9d00
Nalani Daniels <135375665+nalDaniels@users.noreply.github.com>
2023-10-17 17:48:09 -0400

    Update NBAPlayersCSV.py

--- NBAPlayersCSV.py
+++ NBAPlayersCSV.py
@@ -5,7 +5,7 @@ import json
 import requests
 import csv
 
-# Get JSON data from NBA players API using requests
+# Make a GET request to the NBA API and get back JSON data
 url = "https://free-nba.p.rapidapi.com/players"
 
 
@@ -14,6 +14,7 @@ headers = {'X-RapidAPI-Key': 'Enter API Key'}
 
 response = requests.request("GET", url, headers=headers)
 
+# Print response status code
 print(response)
 
 # Print data
@@ -27,7 +28,8 @@ player_info = json.loads(response.text)
 
 # Delete meta data
 del player_info["meta"]
- 
+
+# Print dictionary
 print(player_info)
 
 #Find out how many players are in file
@@ -41,19 +43,19 @@ numofplayers = len(player_info["data"])
 # print(player_info["data"][0]["position"]) #Expecting C
 # print(player_info["data"][0]["team"]["full_name"]) #Expecting Indiana Pacers
 
-#If no position found, input Position Not Known
+# If no position found, input 'Position Not Known'
 for i in range(numofplayers):
     if bool(player_info["data"][i]["position"]) == 0:
         player_info["data"][i]["position"] = "Position Not Known"
 
 
-#Create function that will make csv file of all NBA players
+# Create function that will make csv file of all NBA players
 def Players_CSV():
     # Set fieldnames
     fieldnames = ['Player ID', 'First Name', 'Last Name', 'Position', 'Team Name']
     # Create rows
     rows = []
-    #Iterate through list of player info
+    # Iterate through list of player info
     for i in range(numofplayers):
     # Set row data to columns
         rows.append({"Player ID": player_info["data"][i]["id"],
@@ -66,9 +68,9 @@ def Players_CSV():
     with open('NBAPlayers.csv', 'w', encoding='UTF8', newline='') as file:
         # Set fieldnames variable to fieldnames
         writer = csv.DictWriter(file, fieldnames=fieldnames)
-        #writes fieldnames to header
+        # Writes fieldnames to header
         writer.writeheader()
-        # writes rows using row data
+        # Writes rows using row data
         writer.writerows(rows)
 
     print("CSV created")
