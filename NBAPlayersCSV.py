77360145763ed6224d9c9e21efaa9ac55dea095d
Nalani Daniels <135375665+nalDaniels@users.noreply.github.com>
2023-10-17 21:42:16 -0400

    Update NBAPlayersCSV.py

--- NBAPlayersCSV.py
+++ NBAPlayersCSV.py
@@ -5,72 +5,80 @@ import json
 import requests
 import csv
 
-# Make a GET request to the NBA API and get back JSON data
+
+# Get JSON data from NBA players API using requests
 url = "https://free-nba.p.rapidapi.com/players"
 
 
-headers = {'X-RapidAPI-Key': 'Enter API Key'}
+headers = {'X-RapidAPI-Key': '20aaee672fmsh52f6148adda23d3p1b9f4fjsne8316faadb28'}
+
+# Define what page of data you start on and how many players you want to see per request. This will create 52 pages
+params = {"page": '0', "per_page": '100'}
+# Start a page 1
+current_page = 1
+
+# Create an empty list of players to append to later
+players = []
+
+#Create a while loop to iterate through 52 pages of information pulled from the API
+while current_page < 53:
+    # Send a get request for each page
+    response = requests.request("GET", url, headers=headers, params=params)
+    # Increment the pages by 1 to send a request for the next page
+    current_page += 1
+    # Update the param page key to the current page
+    params["page"]=current_page
+    # Transfrom the JSON string into a dictionary
+    player_info = json.loads(response.text)
+    # Iterate through the dictionary 
+    for player in player_info["data"]:
+        # Add players to the players from previous pages
+        players.append(player)
+
+# Get number of players and ensure you have the correct amount imported 
+numofplayers = len(players)
+if numofplayers != 5130:
+    print("Make sure you got all the pages of API data")
+else:
+    print("Great! You were able to iterate through all pages.")
+
+# print(players)
 
 
-response = requests.request("GET", url, headers=headers)
-
-# Get response code 
-print(response.status_code)
-
-# Print data
-print(response.text)
-
-# Find data type of response
-print(type(response.text))
-
-# Turn string into dictionary
-player_info = json.loads(response.text)
-
-# Delete meta data
-del player_info["meta"]
-
-# Print dictionary
-print(player_info)
-
-#Find out how many players are in file
-numofplayers = len(player_info["data"])
-
-
-# Access data
-# print(player_info["data"][0]["id"]) #Expecting 14
-# print(player_info["data"][0]["first_name"]) #Expecting Ike
-# print(player_info["data"][0]["last_name"]) #Expecting Anigbogu
-# print(player_info["data"][0]["position"]) #Expecting C
-# print(player_info["data"][0]["team"]["full_name"]) #Expecting Indiana Pacers
-
-# If no position found, input 'Position Not Known'
+#If no position found, input Position Not Known
 for i in range(numofplayers):
-    if bool(player_info["data"][i]["position"]) == 0:
-        player_info["data"][i]["position"] = "Position Not Known"
+    if bool(players[i]["position"]) == 0:
+        players[i]["position"] = "Position Not Known"
 
+#Access Data
+print(players[0]["id"]) #Expecting 14
+print(players[0]["first_name"]) #Expecting Ike
+print(players[0]["last_name"]) #Expecting Anigbogu
+print(players[0]["position"]) #Expecting C
+print(players[0]["team"]["full_name"]) #Expecting Indiana Pacers
 
-# Create function that will make csv file of all NBA players
+#Create function that will make csv file of all NBA players
 def Players_CSV():
     # Set fieldnames
     fieldnames = ['Player ID', 'First Name', 'Last Name', 'Position', 'Team Name']
     # Create rows
     rows = []
-    # Iterate through list of player info
+    #Iterate through list of player info
     for i in range(numofplayers):
     # Set row data to columns
-        rows.append({"Player ID": player_info["data"][i]["id"],
-        "First Name": player_info["data"][i]["first_name"],
-        "Last Name": player_info["data"][i]["last_name"],
-        "Position": player_info["data"][i]["position"],
-        "Team Name": player_info["data"][i]["team"]["full_name"]})
+        rows.append({"Player ID": players[i]["id"],
+        "First Name": players[i]["first_name"],
+        "Last Name": players[i]["last_name"],
+        "Position": players[i]["position"],
+        "Team Name": players[i]["team"]["full_name"]})
     
     # Write to csv file
-    with open('NBAPlayers.csv', 'w', newline='') as file:
+    with open('NBAPlayers.csv', 'w', encoding='UTF8', newline='') as file:
         # Set fieldnames variable to fieldnames
         writer = csv.DictWriter(file, fieldnames=fieldnames)
-        # Writes fieldnames to header
+        #writes fieldnames to header
         writer.writeheader()
-        # Writes rows using row data
+        # writes rows using row data
         writer.writerows(rows)
 
     print("CSV created")
